32c31d64f36109727dfa828007021df9dfec78ac
Kerri L OGorman <kerri.l.ogorman@gmail.com>
2020-07-31 22:28:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,3 +14,20 @@ def key_for_min_value(name_hash)
 end
 
 key_for_min_value(name_hash)
+
+
+def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
+  name_hash.each do |a, b|
+    if lowest_value == nil || b < lowest_value
+      lowest_value = b
+      lowest_key = a
+    end
+  end
+ lowest_key
+end
+
+
+
+      end
